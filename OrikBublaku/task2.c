b0d92ecc9d1e3d818ac16ffb041f14b707fe85c3
orik bublaku <orikbublaku12@gmail.com>
2026-04-14 14:07:38 +0200

    Task 2 C program

---
+++ OrikBublaku/task2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World\n");
+    printf("My name is Orik\n");
+
+    int a = 10;
+    int b = 5;
+    int sum = a + b;
+
+    printf("The sum of %d and %d is %d\n", a, b, sum);
+
+    return 0;
+}
