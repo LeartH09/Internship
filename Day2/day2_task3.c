306a3a25ff5057ccd95df20393a3dadb35d04470
gurigjakova <gjakovaguri123@gmail.com>
2026-04-15 14:12:04 +0200

    Task3

---
+++ Day2/day2_task3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+    int dita;
+
+    printf("Shkruani nje numer (1-7): ");
+    scanf("%d", &dita);
+
+    switch (dita) {
+        case 1:
+            printf("E Hene - dite pune\n");
+            break;
+        case 2:
+            printf("E Marte - dite pune\n");
+            break;
+        case 3:
+            printf("E Merkure - dite pune\n");
+            break;
+        case 4:
+            printf("E Enjte - dite pune\n");
+            break;
+        case 5:
+            printf("E Premte - dite pune\n");
+            break;
+        case 6:
+            printf("E Shtune - fundjave\n");
+            break;
+        case 7:
+            printf("E Diel - fundjave\n");
+            break;
+        default:
+            printf("Input i pavlefshem! Ju lutem jepni nje numer nga 1 deri ne 7.\n");
+            break;
+    }
+
+    return 0;
+}
