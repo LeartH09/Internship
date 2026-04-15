27ac6812ece1cef3ba393a2d08808da8c4f73864
Nejla Lajci <nl277@bgt.schoo>
2026-04-15 11:28:57 +0200

    day2_task2

---
+++ Day2/day2_task2.c
@@ -0,0 +1,31 @@
+
+int main() 
+{
+    int rezultati;
+
+    printf("Shkruani rezultatin (0-100): ");
+    scanf("%d", &rezultati);
+
+    if (rezultati < 0 || rezultati > 100) 
+    {
+    printf("Rezultati i pavlefshem. Ju lutem shkruani nje numer midis 0 dhe 100.\n");
+    }
+    else if(rezultati >= 90) 
+    {
+        printf("Shkelqyeshem.\n");
+    }
+    else if(rezultati >= 75)  
+    {
+        printf("Shume mire.\n");
+    } 
+    else if(rezultati >= 50)
+    {
+        printf("Mire.\n");
+    } 
+    else 
+    {
+        printf("Ne permiresim.\n");
+    }
+
+    return 0;
+}
