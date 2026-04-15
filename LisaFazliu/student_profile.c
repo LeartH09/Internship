f0d930244ea284d64461e8481504a75c09e56908
Lisa Fazliu <lf273@bgt.school>
2026-04-15 09:19:19 +0200

    test

---
+++ LisaFazliu/student_profile.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Name: Lisa Fazliu");
+    printf("Age: 17\n");
+    printf("School: BGT\n");
+    printf("Interests: Programming\n");
+
+    return 0;
+}
