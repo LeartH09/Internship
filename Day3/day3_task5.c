6908e69fccbdf3a7c2db00991e8d1bf439b2759b
Nejla Lajci <nl277@bgt.schoo>
2026-04-17 08:52:32 +0200

    day3_task5

---
+++ Day3/day3_task5.c
@@ -0,0 +1,49 @@
+int main() 
+{
+    enum Level 
+    {
+        BEGINNER = 1, 
+        INTERMEDIATE, 
+        ADVANCED,     
+        EXPERT        
+    };
+
+    int choice;
+    int validCount = 0;
+    int invalidCount = 0;
+
+    for (int i = 1; i <= 3; i++) 
+    {
+        printf("\nZgjedh nje nivel (1-4): ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case BEGINNER: 
+                printf("Ti je fillestar.\n");
+                validCount++;
+                break;
+            case INTERMEDIATE:
+                printf("Ti je mesatar.\n");
+                validCount++;
+                break;
+            case ADVANCED: 
+                printf("Ti je i avancuar.\n");
+                validCount++;
+                break;
+            case EXPERT:
+                printf("Ti je ekspert.\n");
+                validCount++;
+                break;
+            default:
+                printf("Zgjedhje e pavlefshme!\n");
+                invalidCount++;
+                break;
+        }
+    }
+
+    printf("\nStatistikat\n");
+    printf("Zgjedhje valide: %d\n", validCount);
+    printf("Zgjedhje pavlefshme: %d\n", invalidCount);
+
+    return 0;
+}
