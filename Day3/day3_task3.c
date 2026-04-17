89f845be707ef92e722692211288f16e071316c1
gurigjakova <gjakovaguri123@gmail.com>
2026-04-17 08:27:56 +0200

    Day3 - Task 3

--- Day3/day3_task3.c
+++ Day3/day3_task3.c
@@ -2,40 +2,39 @@
 
 int main() {
     int start, end;
-    int countCat1 = 0, countCat2 = 0;
-    int sumCat1 = 0, sumCat2 = 0;
+    int count1 = 0, sum1 = 0;
+    int count2 = 0, sum2 = 0;
 
-    printf("Enter the start of the interval: ");
+    printf("Jepni fillimin e intervalit: ");
     scanf("%d", &start);
-    printf("Enter the end of the interval: ");
+    printf("Jepni fundin e intervalit: ");
     scanf("%d", &end);
 
     if (start > end) {
-        printf("Error: Invalid interval! Start must be less than or equal to end.\n");
+        printf("Gabim: Fillimi nuk mund te jete me i madh se fundi!\n");
         return 1;
     }
 
     for (int i = start; i <= end; i++) {
         if (i % 3 == 0) {
-            countCat1++;
-            sumCat1 += i;
+            count1++;
+            sum1 += i;
         } 
         else if (i % 5 == 0) {
-            countCat2++;
-            sumCat2 += i;
+            count2++;
+            sum2 += i;
         }
     }
 
-    printf("\n--- Comparison Results ---\n");
-    printf("Category 1 (Divisible by 3): Count = %d, Sum = %d\n", countCat1, sumCat1);
-    printf("Category 2 (Divisible by 5 but not 3): Count = %d, Sum = %d\n", countCat2, sumCat2);
+    printf("\nKategoria 1 (Pjestuesit e 3-shit): Numeruesi = %d, Shuma = %d\n", count1, sum1);
+    printf("Kategoria 2 (Pjestuesit e 5-ses pa 3-shin): Numeruesi = %d, Shuma = %d\n", count2, sum2);
 
-    if (sumCat1 > sumCat2) {
-        printf("Result: Category 1 has a larger sum.\n");
-    } else if (sumCat2 > sumCat1) {
-        printf("Result: Category 2 has a larger sum.\n");
+    if (sum1 > sum2) {
+        printf("Rezultati: Shuma e Kategorise 1 eshte me e madhe.\n");
+    } else if (sum2 > sum1) {
+        printf("Rezultati: Shuma e Kategorise 2 eshte me e madhe.\n");
     } else {
-        printf("Result: Both sums are equal.\n");
+        printf("Rezultati: Shumat jane te barabarta.\n");
     }
 
     return 0;
